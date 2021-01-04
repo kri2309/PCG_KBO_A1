ba4a745a2b6838c39e5f40f67d6a0c24d8709b6b
kri2309 <kriskbo@gmail.com>
2021-01-04 23:26:03 +0100

    Fixed issue in AA1
    Assigned materials programmatically unlike before

--- PCG_KBO/Assets/Resources/Scripts/TriangleGen.cs
+++ PCG_KBO/Assets/Resources/Scripts/TriangleGen.cs
@@ -31,6 +31,41 @@ public class TriangleGen : MonoBehaviour
         meshFilter.mesh = meshBuilder.CreateMesh();
 
         MeshRenderer meshRenderer = this.GetComponent<MeshRenderer>();
+        AddMaterials();
         meshRenderer.materials = allMaterials.ToArray();
+    }
+
+    private void AddMaterials()
+    {
+
+        Material yellow = new Material(Shader.Find("Specular"));
+        yellow.color = Color.yellow;
+
+        Material white = new Material(Shader.Find("Specular"));
+        white.color = Color.white;
+
+        Material magenta = new Material(Shader.Find("Specular"));
+        magenta.color = Color.magenta;
+
+        Material red = new Material(Shader.Find("Specular"));
+        red.color = Color.red;
+
+        Material blue = new Material(Shader.Find("Specular"));
+        blue.color = Color.blue;
+
+        Material green = new Material(Shader.Find("Specular"));
+        green.color = Color.green;
+
+     
+
+
+
+        allMaterials = new List<Material>();
+        allMaterials.Add(white);
+        allMaterials.Add(blue);
+        allMaterials.Add(green);
+        allMaterials.Add(magenta);
+        allMaterials.Add(yellow);
+        allMaterials.Add(red);
     }
 }
